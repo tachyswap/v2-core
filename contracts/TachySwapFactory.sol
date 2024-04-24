d3db2871fc2d757e6533c2d4570386e1cad1c92a
tachyswap <ocelotdex@gmail.com>
2024-04-24 12:50:28 +0800

    ~

---
+++ contracts/TachySwapFactory.sol
@@ -0,0 +1,56 @@
+pragma solidity =0.5.16;
+
+import './interfaces/ITachySwapFactory.sol';
+import './TachySwapPair.sol';
+
+contract TachySwapFactory is ITachySwapFactory {
+    bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(TachySwapPair).creationCode)); // INIT_CODE_PAIR_HASH
+
+    address public feeTo;
+    address public feeToSetter;
+
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    constructor(address _feeToSetter) public {
+        feeToSetter = _feeToSetter;
+    }
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        require(tokenA != tokenB, 'TachySwap: IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'TachySwap: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'TachySwap: PAIR_EXISTS'); // single check is sufficient
+        bytes memory bytecode = type(TachySwapPair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        ITachySwapPair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function setFeeTo(address _feeTo) external {
+        require(msg.sender == feeToSetter, 'TachySwap: FORBIDDEN');
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, 'TachySwap: FORBIDDEN');
+        feeToSetter = _feeToSetter;
+    }
+    
+    function setSwapFee(address _pair, uint32 _swapFee) external {
+        require(msg.sender == feeToSetter, 'TachySwap: FORBIDDEN');
+        TachySwapPair(_pair).setSwapFee(_swapFee);
+    }
+}
